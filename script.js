f304b70156759e97e6e2e819555a4e3479f469ce
Michael Ammann <mazgch@users.noreply.github.com>
2023-08-27 22:18:04 +0200

    major rework

--- script.js
+++ script.js
@@ -5,182 +5,93 @@ window.onload = function _onload() {
     let chart = undefined;
     let labels = [];
     let datasets = [];
-    let input;
     
-    const table = document.getElementById('table');
-    const scores = document.getElementById('scores');
-    const name = document.getElementById("name");
+    const name = document.getElementById("player");
     const score = document.getElementById("score");
+    const scores = document.getElementById('scores');
     feather.replace();
 
-    const cookieTag = "names=";
-    let names = [ "Player 1", "Player 2" ];
+    const cookieTag = "players=";
+    let players = [ undefined, undefined ]; // default 2 Players 
     document.cookie.split(';').forEach( function _cookie(cookie) {
         try {
             if (cookie.startsWith(cookieTag)) {
-                names = JSON.parse(cookie.substring(cookieTag.length));
+                players = JSON.parse(cookie.substring(cookieTag.length));
             }
         } catch (e) {
         }
     });
     
-    resetPlayers();
+    setPlayers();
     
     window.onunload = function _onunload() {
-        document.cookie = cookieTag + JSON.stringify(names);
+        document.cookie = cookieTag + JSON.stringify(players);
     }
     
-    window.setPlayers = resetPlayers;
+    window.setPlayers = setPlayers;
     
-    function resetPlayers(n) {
-        n = names.length + ((n === undefined) ? 0 : n);
-        names.length = Math.min(7, Math.max(1,n));
+    function setPlayers(n) {
+        n = players.length + ((n === undefined) ? 0 : n);
+        players.length = Math.min(7, Math.max(1,n));
         initChart();
         let newCell = document.createElement("TH");
         name.replaceChildren(name.firstElementChild);
         score.replaceChildren(score.firstElementChild);
-        newCell = document.createElement("TR");
-        scores.replaceChildren(newCell);
-        newCell = newCell.insertCell();    
-        newCell.textContent = "Round 1";
-        for (let p = 0; p < names.length; p ++) {
-            if (!names[p]) names[p] = "Player " + (p+1);
+        scores.replaceChildren();
+        for (let p = 0; p < players.length; p ++) {
             newCell = document.createElement("TH");
-            newCell.textContent = names[p];
-            newCell.setAttribute("edittype", "text");
-            newCell.style.backgroundColor = datasets[p].backgroundColor;
-/*
-            let span = document.createElement("SPAN");
-            span.textContent = "🎨";
-            span.setAttribute("iconright", "");
-            function chooseColor(e) {
-                var colorInput = document.createElement('input');
-                colorInput.type = 'color';
-                colorInput.addEventListener('input', function(event) {
-                    alert('Selected Color: ' + event.target.value);
-                });
-                span.appendChild(colorInput);
-                colorInput.style.opacity = 0;
-                colorInput.click();
+            let inputCell = document.createElement("INPUT");
+            inputCell.type = "text";
+            inputCell.value = players[p].name;
+            inputCell.onkeyup = updateScore;
+            inputCell.style.borderColor = players[p].color;
+            inputCell.style.backgroundColor = players[p].background;
+            inputCell.onchange = function _onchangeName(e) {
+                const name = inputCell.value;
+                players[p].name = name;
+                datasets[p].label = name;
+                chart.update();
             }
-            span.onclick = chooseColor;
-            newCell.appendChild(span);
-            */
+            newCell.appendChild(inputCell);
+            let spanCell = document.createElement("SPAN");
+            spanCell.className = "iconleft";
+            spanCell.onclick = function _onclickName(e) { 
+                inputCell.focus(); 
+            };
+            spanCell.textContent = "✎";
+            newCell.appendChild(spanCell);
+            spanCell = document.createElement("SPAN");
+            spanCell.className = "iconright";
+            spanCell.textContent = "🎨";
+            let inputColor = document.createElement('INPUT');
+            inputColor.type = 'color';
+            inputColor.style.opacity = "0";
+            inputColor.onchange = function _onchangeColor(e) {
+                const color = inputColor.value;
+                players[p].color = color;
+                datasets[p].borderColor = color;
+                inputCell.style.borderColor = color;
+                const background = "rgba(" + 
+                                  parseInt(color.slice(1, 3), 16) + ", " + 
+                                  parseInt(color.slice(3, 5), 16) + ", " + 
+                                  parseInt(color.slice(5, 7), 16) + ", 0.5)";
+                players[p].background = background;
+                datasets[p].backgroundColor = background; 
+                inputCell.style.backgroundColor = background;
+                chart.update();
+            };
+            spanCell.appendChild(inputColor);
+            spanCell.onclick = function _onclickColor(e) {
+                inputColor.value = datasets[p].borderColor; 
+                inputColor.focus(); 
+            };
+            newCell.appendChild(spanCell);
             name.appendChild(newCell);
             newCell = document.createElement("TH");
             newCell.textContent = 0;
             score.appendChild(newCell);
-            newCell = scores.firstElementChild.insertCell();
-            newCell.setAttribute("edittype", "number");
         }
-        makeCellEditable(scores.rows[0].children[1]);
-    }
-    
-    table.addEventListener('click', function _click(e) {
-        let newCell = e.target;
-        if (input !== newCell) {
-            if (!newCell.hasAttribute("edittype")) {
-                newCell = undefined;
-            }
-            if (input !== undefined) {
-                input.blur()
-                let oldCell = input.parentNode;
-                oldCell.textContent = input.value;
-                input = undefined;
-                nextCell(oldCell); // mekes sure there is new round 
-                updateScore();
-            }
-            if (newCell) {
-                makeCellEditable(newCell);
-            }
-        }
-    });
-     
-    function nextEmptyCell(cell) {
-        if (cell === undefined) return;
-        let nextCell = cell;
-        while (nextCell) {
-            const value = parseInt(nextCell.textContent);
-            const isEmpty = !Number.isInteger(value)
-            if (nextCell.hasAttribute("edittype") && isEmpty)
-                return nextCell;
-            nextCell = nextCell.nextElementSibling;
-        } 
-        nextCell = cell;
-        while (nextCell) {
-            const value = parseInt(nextCell.textContent);
-            const isEmpty = !Number.isInteger(value)
-            if (nextCell.hasAttribute("edittype") && isEmpty)
-                return nextCell;
-            nextCell = nextCell.previousElementSibling;
-        }
-        return nextCell;
-    }
-
-    function nextTableCell(cell) {
-        if (cell === undefined) return;
-        let nextCell = cell.nextElementSibling;
-        do {
-            while (nextCell) {
-                if (nextCell.hasAttribute("edittype"));
-                    return nextCell;
-                nextCell = nextCell.nextElementSibling;
-            } 
-            if (cell.parentNode.nextElementSibling) {
-                nextCell = nextCell.firstElementChild;
-            }
-        } while (nextCell);
-        return nextCell;
-    }
-    
-    function nextCell(cell) {
-        let nextCell;
-        if (cell.parentNode === scores.firstElementChild) {
-            nextCell = nextEmptyCell(cell);
-            if (!nextCell) {   
-                const numRows = scores.rows.length;
-                const newRow = scores.insertRow(0);
-                const newCell = newRow.insertCell();
-                newCell.textContent = "Round " + (numRows+1);
-                for (let p = 0; p < names.length; p++) {
-                    const newCell = newRow.insertCell();
-                    newCell.setAttribute("edittype", "number");
-                }
-                nextCell = newRow.children[1];
-            }
-        } else {
-            nextCell = nextTableCell()
-        }
-        return nextCell;
-    }
-    
-    function makeCellEditable(cell) {
-        if ((cell === undefined) || !cell.hasAttribute("edittype")) return;
-        input = document.createElement('input');
-        input.oldValue = cell.textContent;
-        input.value = input.oldValue;
-        input.type = cell.getAttribute("edittype");
-        if(input.type === "number") {
-            input.pattern = "[0-9]*";
-        }
-        cell.replaceChildren(input);
-        input.focus();
-        input.addEventListener('keydown', function _keydown(e) {
-            if ((e.key === 'Enter') || (e.key === 'Tab')) {
-                e.preventDefault();
-                input.blur()
-                cell.textContent = input.value;
-                input = undefined;
-                makeCellEditable(nextCell(cell));
-                updateScore();
-            } else if (e.key === 'Escape') {
-                e.preventDefault();
-                input.value = input.oldValue;
-            }
-        });
-        if (input.type == "text") {
-            input.setSelectionRange(0,input.value.length);
-        }
+        updateScore(0);
     }
 
     function initChart() {
@@ -191,13 +102,16 @@ window.onload = function _onload() {
         datasets = [];
         labels = [];
         let ctx = document.getElementById('scoreChart').getContext('2d');
-        for (let p = 0; p < names.length; p++) {
+        for (let p = 0; p < players.length; p++) {
+            if (!players[p]) players[p] = { name: "Player " + (p+1) }; 
             datasets[p] = {
-                label: names[p],
+                label: players[p].name,
                 fill: false,
                 tension: 0.1,
                 data: [0]
             };
+            if (players[p].color !== undefined)       datasets[p].borderColor = players[p].color;
+            if (players[p].background !== undefined)  datasets[p].backgroundColor = players[p].background;
         }
         chart = new Chart(ctx, {
             type: 'line',
@@ -218,32 +132,91 @@ window.onload = function _onload() {
                 }
             }
         });
-    }
-
-    function updateScore() {
-        let num = scores.childElementCount;
-        let sum = [];
-        for (let p = 0; p < names.length; p++) {
-            sum[p] = 0;
-            datasets[p].label = names[p] = name.children[p+1].textContent;
+        for (let p = 0; p < players.length; p++) {
+            players[p].color = datasets[p].borderColor;
+            players[p].background = datasets[p].backgroundColor;
         }
-        for (let s = 0; s < num; s++) {
-            labels[s] = s+1;
-            for (let p = 0; p < names.length; p++) {
-                let val = scores.children[(num - 1) - s].children[p+1].textContent;
+    }
+    
+    function updateScore(insertRow = undefined) {
+        let sum = [];
+        for (let p = 0; p < players.length; p++) {
+            sum[p] = 0;
+            if ((insertRow !== undefined) && (scores.firstElementChild)) {
+                let val = scores.firstElementChild.children[p+1].firstElementChild.value;
+                val = parseInt(val);
+                if (!Number.isInteger(val)) insertRow = undefined;
+            }
+        }
+        if (insertRow !== undefined) {
+            const numRows = scores.rows.length;
+            const focusedCell = document.activeElement;
+            const focusedCol = ((focusedCell && focusedCell.parentNode.tagName === "TD") && (focusedCell.cellIndex < players.length)) ? focusedCell.cellIndex + 1 : 1;
+            const newRow = scores.insertRow(0);
+            const newCell = newRow.insertCell();
+            newCell.textContent = "Round " + (numRows+1);
+            for (let p = 0; p < players.length; p++) {
+                const newCell = newRow.insertCell();
+                let inputCell = document.createElement("INPUT");
+                inputCell.type = "number";
+                inputCell.inputmode = "numeric";
+                inputCell.pattern = "[0-9]*";
+                inputCell.nextRowIx = (p + 1) % players.length;
+                inputCell.onfocus = function _onfocus(e) {
+                    inputCell.oldValue = inputCell.value;
+                }
+                inputCell.onblur = function _onblur(e) {
+                    if (!inputCell.checkValidity()) {
+                        inputCell.value = inputCell.oldValue;
+                        // refocus input field at end of call stack if content invalid
+                        // setTimeout(() => inputCell.focus(), 0);
+                    } else {
+                        updateScore(inputCell.nextRowIx);
+                    }
+                }
+                inputCell.onkeyup = function _onkeyup(e) {
+                    if (e.key === 'Enter') {
+                        e.preventDefault();
+                        newRow.children[inputCell.nextRowIx+ 1].firstElementChild.focus();
+                        inputCell.blur();
+                    } else if (e.key === 'Escape') {
+                        e.preventDefault();
+                        inputCell.value = inputCell.oldValue;
+                        inputCell.blur();
+                    } else {
+                        updateScore();
+                    }
+                };
+                newCell.appendChild(inputCell);
+                let spanCell = document.createElement("SPAN");
+                spanCell.className = "iconleft";
+                spanCell.onclick = function _click() { 
+                    inputCell.focus();
+                    inputCell.setSelectionRange(0, inputCell.value.length);
+                };
+                spanCell.textContent = "✎";
+                newCell.appendChild(spanCell);
+            }
+            newRow.children[insertRow + 1].firstElementChild.focus();
+            
+        }
+        let numRows = scores.rows.length;
+        for (let r = 0; r < numRows; r++) {
+            labels[r] = r+1;
+            for (let p = 0; p < players.length; p++) {
+                let val = scores.rows[(numRows - 1) - r].children[p+1].firstElementChild.value;
                 val = parseInt(val);
                 if (Number.isInteger(val)) {
                     sum[p] += val;
-                    datasets[p].data[s] = sum[p];
-                } else if (s + 1 < num) {
-                    datasets[p].data[s] = sum[p];
+                    datasets[p].data[r] = sum[p];
+                } else if (r + 1 < numRows){
+                    datasets[p].data[r] = sum[p];
                 }
             }
         }
-        for (let p = 0; p < names.length; p++) {
-            score.children[p+1].innerHTML = sum[p];
+        for (let p = 0; p < players.length; p++) {
+            score.children[p+1].textContent = sum[p];
         }
         chart.update();
     }
-
 };
